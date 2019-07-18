c4aac268e38bcbbd7b23ce49c3244c81635f419f
Anton Krug <anton.krug@microchip.com>
2019-07-18 17:17:36 +0100

    renamed the build to release and timestamp can be given from enviroment

--- build_all_platforms.sh
+++ build_all_platforms.sh
@@ -24,31 +24,32 @@ go get github.com/dustin/go-humanize
 echo "Generating  assets"
 go generate
 
-echo "Creating clean build folder"
-mkdir -p build
-rm -r ./build
-mkdir -p build
+TS="${TS:-`date +"%Y%m%d-%H%M%S"`}" 
+echo "Timestamp is: $TS"
 
-TS=`date +"%Y%m%d-%H%M%S"`
+echo "Creating clean build folder"
+mkdir -p release
+rm -r ./release
+mkdir -p release
 
 echo "Building Windows"
-GOOS=windows GOARCH=amd64 go build -o build/ead-windows-x86-64-$TS.exe
-GOOS=windows GOARCH=386 go build -o build/ead-windows-x86-32-$TS.exe
-GOOS=windows GOARCH=arm go build -o build/ead-windows-arm-$TS.exe
+GOOS=windows GOARCH=amd64 go build -o release/ead-windows-x86-64-$TS.exe
+GOOS=windows GOARCH=386 go build -o release/ead-windows-x86-32-$TS.exe
+GOOS=windows GOARCH=arm go build -o release/ead-windows-arm-$TS.exe
 
 echo "Building Linux"
-GOOS=linux GOARCH=amd64 go build -o build/ead-linux-x86-64-$TS
-GOOS=linux GOARCH=386 go build -o build/ead-linux-x86-32-$TS
-GOOS=linux GOARCH=arm64 go build -o build/ead-linux-arm-64-$TS
-GOOS=linux GOARCH=arm go build -o build/ead-linux-arm-32-$TS
+GOOS=linux GOARCH=amd64 go build -o release/ead-linux-x86-64-$TS
+GOOS=linux GOARCH=386 go build -o release/ead-linux-x86-32-$TS
+GOOS=linux GOARCH=arm64 go build -o release/ead-linux-arm-64-$TS
+GOOS=linux GOARCH=arm go build -o release/ead-linux-arm-32-$TS
 
 echo "Building FreeBSD"
-GOOS=freebsd GOARCH=amd64 go build -o build/ead-freebsd-x86-64-$TS
-GOOS=freebsd GOARCH=386 go build -o build/ead-freebsd-x86-32-$TS
+GOOS=freebsd GOARCH=amd64 go build -o release/ead-freebsd-x86-64-$TS
+GOOS=freebsd GOARCH=386 go build -o release/ead-freebsd-x86-32-$TS
 
 echo "Building MacOS"
-GOOS=darwin GOARCH=amd64 go build -o build/ead-macos-x86-64-$TS
-GOOS=darwin GOARCH=386 go build -o build/ead-macos-x86-32-$TS
+GOOS=darwin GOARCH=amd64 go build -o release/ead-macos-x86-64-$TS
+GOOS=darwin GOARCH=386 go build -o release/ead-macos-x86-32-$TS
 
 echo
 echo "Finished artifacts:"
